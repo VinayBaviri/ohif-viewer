0e87fbebcb633a80639f9b9b29a56345edd9d133
Hari Baviri <hari.baviri@sweken.com>
2021-09-07 13:09:45 +0530

    Forming JSON dynamically

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 Doc links :: https://docs.ohif.org/deployment/
 
 **STEPS**
-- Make sure that you are using updated version of OHIF viewer in the `ohif.html` file.
+- Make sure that you are using updated version of OHIF viewer in the `ohif.html` file. Currently i'm using `"https://unpkg.com/@ohif/viewer@4.9.22/dist/index.umd.js"`. [Check here](https://www.npmjs.com/package/@ohif/viewer "Check here").
 - To server ohif.html file locally, we are using [parcel](https://www.npmjs.com/package/parcel "parcel") npm package.
-- Start OHIF viewer by `npm run ohif`. It will server `ohif.html` file locally in the port `8888`. local url :: `http://localhost:8888/`.
-- To view local dicom files directly, naviage to `/local` and browser internal `.dcm` files.
+- Start OHIF viewer, run  `npm run ohif`. It will server `ohif.html` file locally in the port `8888`. local url :: `http://localhost:8888/`.
+- To view local dicom files directly, naviage to `/local` and browser internal `DICOM` files.
 - To view remote file hosted on publicy available url, pass the json in the structured way.
-- To get sample configured JSON locallly, run local node server `npm run start`.
+- To get  configured JSON locallly, run local node server `npm run start`.
 - Browse `http://localhost:6789/view` to view sample configured JSON.
 - Append local server json fetch url to the `url` param of ohif viewer. For example `http://localhost:8888/viewer?url=http://localhost:6789/view`.
 - You can able to see the dicom file with the OHIF viewer support.
+- To extend for any remotely hosted public dicom file, pass query param `imageUrl` at the end.
+- For example `http://localhost:8888/viewer?url=http://localhost:6789/view?imageUrl=https://s3.amazonaws.com/lury/MRStudy/1.2.840.113619.2.5.1762583153.215519.978957063.124.dcm`.
+- We are getting metadata from the dicom file and extracting the required data points and then forming the JSON. So, every public dicom file we can render in OHIF viewer.
 
--- server.js
+++ server.js
@@ -1,72 +1,97 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
+const dicomParser = require('dicom-parser');
+const { v4: uuidv4 } = require('uuid');
 
-const date = new Date().getTime();
-console.log('-------- date :: ' + date);
-const OHIFJSON = {
-  "studies": [
-    {
-      "StudyInstanceUID": "12345678910",
-      "StudyDescription": "BRAIN SELLA",
-      "series": [
-        {
-          "SeriesInstanceUID": "9876543210",
-          "SeriesDescription": "series Description",
-          "SeriesDate": date,
-          "SeriesTime": "120318",
-          "instances": [
-            {
-              "metadata": {
-                "Columns": 512,
-                "Rows": 512,
-                "InstanceNumber": 0,
-                "AcquisitionNumber": 100,
-                "PhotometricInterpretation": "MONOCHROME2",
-                "BitsAllocated": 16,
-                "BitsStored": 16,
-                "PixelRepresentation": 1,
-                "SamplesPerPixel": 1,
-                "PixelSpacing": [
-                  0.390625,
-                  0.390625
-                ],
-                "HighBit": 15,
-                "ImageOrientationPatient": [
-                  0,
-                  1,
-                  0,
-                  0,
-                  0,
-                  -1
-                ],
-                "ImagePositionPatient": [
-                  11.600000,
-                  -92.500000,
-                  98.099998
-                ],
-                "FrameOfReferenceUID": "1.2.3.4.5.6.7.8.9",
-                "ImageType": [
-                  "ORIGINAL",
-                  "PRIMARY",
-                  "OTHER"
-                ],
-                "Modality": "MR",
-                "SOPInstanceUID": "147258369",
-                "SeriesInstanceUID": "147963258",
-                "StudyInstanceUID": "147852369"
-              },
-              "url": "dicomweb://s3.amazonaws.com/lury/MRStudy/1.2.840.113619.2.5.1762583153.215519.978957063.124.dcm"
-            }
-          ]
-        }
-      ]
-    }
-  ]
+
+
+app.get('/view', async (req, res, next) => {
+  try {
+    const imageUrl = req.query.imageurl ? req.query.imageurl :
+      'https://s3.amazonaws.com/lury/MRStudy/1.2.840.113619.2.5.1762583153.215519.978957063.124.dcm';
+    const response = await getOhifRenderJson(imageUrl, next)
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+});
+
+async function getOhifRenderJson(dicomFileUrl, next) {
+  const dicomFileMetaData = await getDicomFileMetadata(dicomFileUrl, next)
+  return formJson(dicomFileMetaData, dicomFileUrl)
 }
 
-app.get('/view', (req, res, next) => {
-  res.send(OHIFJSON);
-});
+async function getDicomFileMetadata(dicomFileUrl, next) {
+  const dicomFileUrlBufferData = await axios.get(dicomFileUrl, { responseType: 'arraybuffer' }); // getting arrbuffer data of dicom image from url;
+  const diocmFileMetaData = dicomParser.parseDicom(dicomFileUrlBufferData.data); // parseDicom expects buffer[] data;
+  return diocmFileMetaData;
+}
+
+function getArrayByString(value) {
+  const rawString = String.raw`${value}`;
+  const newArray = rawString.split('\\');
+  return newArray;
+}
+
+function formJson(dataSet, dicomFileUrl) {
+  const frameUid = uuidv4();
+  const dicomUrl = dicomFileUrl.replace(/https?/, 'dicomweb'); // Replacing https protocol with dicomweb;
+  const ImageType = getArrayByString(dataSet.string('x00080008')); // ImageType data-point in metadata;
+  const PixelSpacing = getArrayByString(dataSet.string('x00280030')); // PixelSpacing data-point in metadata;
+  const AcquisitionNumber = parseInt(dataSet.string('x00200012')) ? parseInt(dataSet.string('x00200012')) : 0;
+  const ImageOrientationPatient = dataSet.string('x00200037') ? getArrayByString(dataSet.string('x00200037')) : [0, 1, 0, 0, 0, -1];
+  const ImagePositionPatient = dataSet.string('x00200032') ? getArrayByString(dataSet.string('x00200032')) : [11.600000, -92.500000, 98.099998];
+  const configJSON = {
+    "studies": [
+      {
+        "StudyDate": dataSet.string('x00080020'),
+        "StudyTime": dataSet.string('x00080030'),
+        "StudyInstanceUID": dataSet.string('x0020000d'),
+        "StudyDescription": dataSet.string('x00081030'),
+        "series": [
+          {
+            "Modality": dataSet.string('x00080060'),
+            "SeriesDate": dataSet.string('x00080021'),
+            "SeriesTime": dataSet.string('x00080031'),
+            "SeriesDescription": dataSet.string('x0008103e'),
+            "SeriesInstanceUID": dataSet.string('x0020000e'),
+            "SeriesNumber": parseInt(dataSet.string('x00200011')),
+            "instances": [
+              {
+                "url": dicomUrl,
+                "metadata": {
+                  "ImageType": ImageType,
+                  "PixelSpacing": PixelSpacing,
+                  "FrameOfReferenceUID": frameUid,
+                  "AcquisitionNumber": AcquisitionNumber,
+                  "ImagePositionPatient": ImagePositionPatient,
+                  "ImageOrientationPatient": ImageOrientationPatient,
+
+                  "Rows": dataSet.uint16('x00280010'),
+                  "Columns": dataSet.uint16('x00280011'),
+                  "HighBit": dataSet.uint16('x00280102'),
+                  "Modality": dataSet.string('x00080060'),
+                  "BitsStored": dataSet.uint16('x00280101'),
+                  "BitsAllocated": dataSet.uint16('x00280100'),
+                  "SOPInstanceUID": dataSet.string('x00080018'),
+                  "SamplesPerPixel": dataSet.uint16('x00280002'),
+                  "StudyInstanceUID": dataSet.string('x0020000d'),
+                  "SeriesInstanceUID": dataSet.string('x0020000e'),
+                  "PixelRepresentation": dataSet.uint16('x00280103'),
+                  "InstanceNumber": parseInt(dataSet.string('x00200013')),
+                  "PhotometricInterpretation": dataSet.string('x00280004'),
+                },
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+  return configJSON;
+}
 
 app.get('/', (req, res) => {
   res.send('welcome to localhost node server at 6789 port')
